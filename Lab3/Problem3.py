8d10a9cfd9fa692394ac69da2f67079345aa8c23
sabrupu <sabrupu18@gmail.com>
2018-10-08 15:46:50 -0400

    Commit Progress on Lab 3 Problem 3.

--- Lab3/Problem3.py
+++ Lab3/Problem3.py
@@ -4,3 +4,23 @@
 # That month has 28 days (29 on a leap year!)
 # Enter a month: 10
 # That month has 31 days!
+
+
+def days_in_month(month):
+    if month == 4 or month == 6 or  month == 11 or \
+        month == 9 or month == 11:
+        return 30
+    elif month == 2:
+        return 28
+    else:
+        return 31
+
+
+def main():
+    month = int(input('Enter a month\'s number: '))
+
+    days = days_in_month(month)
+
+    print(f'There are {days} days in month {month} !')
+
+main()
