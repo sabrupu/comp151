42c1659adceca0ab862a5eaa68c247b111d2294f
sabrupu <sabrupu18@gmail.com>
2018-10-09 23:54:19 -0400

    Begin PAL Assignment 1.

---
+++ PAL/Assignment1/Assignment1.py
@@ -0,0 +1,32 @@
+# Write a program to create a customer’s bill for a company. The company sells only five
+# different products: TV, VCR, Remote Controller, CD Player and Tape Recorder. The unit
+# prices are $400.00, $220, $35.20, $300.00, and $150.00, respectively. The program must
+# read the quantity of each piece of equipment purchased from the keyboard. It then
+# calculates the cost of each item, the subtotal, and the total cost after an 8.25% sales tax.
+# The input data consist of a set of integers representing the quantities of each item sold.
+# These integers must be input into the program in a user friendly way; that is, the program
+# must prompt the user for each quantity as shown below. The numbers in boldface show
+# the user’s answers.
+#
+#     • How Many TVs Were Sold? 3
+#     • How Many VCRs Were Sold? 5
+#     • How Many Remote Controllers Were Sold? 1
+#     • How Many CDs Were Sold? 2
+#     • How Many Tape Recorders Were Sold? 4
+#
+# The format for the output from the program follows:
+#
+#   QTY     DESCRIPTION         UNIT PRICE      TOTAL PRICE
+#   ---     -----------         ----------      -----------
+#   XX      TV                  400.00            XXXX.XX
+#   XX      VCR                 220.00            XXXX.XX
+#   XX      REMOTE CTRLR         35.20            XXXX.XX
+#   XX      CD PLAYER           300.00            XXXX.XX
+#   XX      TAPE RECORDER       150.00            XXXX.XX
+#                                                 ---------
+#                               SUBTOTAL          XXXXX.XX
+#                               TAX                XXXX.XX
+#                               TOTAL             XXXXX.XX
+
+
+# TODO: Implement.
