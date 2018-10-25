167f492617775f36543a66741efc3ec05a420bb3
sabrupu <sabrupu18@gmail.com>
2018-10-25 00:15:59 -0400

    Commit Assignment1 progress.

--- PAL/Assignment1/Assignment1.py
+++ PAL/Assignment1/Assignment1.py
@@ -28,5 +28,51 @@
 #                               TAX                XXXX.XX
 #                               TOTAL             XXXXX.XX
 
+print('Assignment 1')
+print()
 
-# TODO: Implement.
+# tv        = int(input('How many TVs were sold? '))
+# vcr       = int(input('How many VCRs were sold? '))
+# rem_cont = int(input('How many remote controllers were sold? '))
+# cd        = int(input('How many CDs were sold? '))
+# tape_rec  = int(input('How many tape recorders were sold? '))
+#
+#
+# tv_sold          = tv       * 400
+# vcr_sold         = vcr      * 220
+# rem_cont_sold    = rem_cont * 35.20
+# cd_sold          = cd       * 300
+# tape_rec_sold    = tape_rec * 150
+# sum              = tv_sold + vcr_sold + rem_cont_sold + cd_sold + tape_rec_sold
+#
+#
+# print(f'Total: {tv_sold}')
+# print(f'Total: {vcr_sold}')
+# print(f'Total: {rem_cont_sold}')
+# print(f'Total: {cd_sold}')
+# print(f'Total: {tape_rec_sold}')
+# print(f'Overall sum: {sum}')
+
+
+def main():
+    # Cost of items
+    tv = 400.00
+    vcr = 200.00
+    rem_cont = 35.20
+    cd = 300.00
+    tape_rec = 150.00
+
+    # Get user input
+    tv_sold = int(input('How many TVs were sold? '))
+    vcr_sold = int(input('How many VCRs were sold? '))
+    rem_cont_sold = int(input('How many remote controllers were sold? '))
+    cd_sold = int(input('How many CDs were sold? '))
+    tape_rec_sold = int(input('How many tape recorders were sold? '))
+
+    # print('QTY\t DESCRIPTION\t UNIT PRICE\t TOTAL PRICE')
+    # print('---\t -----------\t ----------\t -----------')
+    # print(f' {tv_sold}\t TV\t\t {tv}\n {vcr_sold}\t VCR\t\t {vcr}\n {rem_cont_sold}\t REMOTE CTRL'
+    #       f'\t\t {rem_cont}\n {cd_sold}\t CD PLAYER\t\t {cd}\n {tape_rec_sold}\t TAPE RECORDER\t\t {tape_rec}')
+
+
+main()
