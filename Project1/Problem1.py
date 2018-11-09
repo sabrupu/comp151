19ea806c3b8d5c022d905f9f4cb2cc3a7cf92a3a
sabrupu <sabrupu18@gmail.com>
2018-11-09 00:45:01 -0500

    Add 'deal' function, along with other progress.

--- Project1/Problem1.py
+++ Project1/Problem1.py
@@ -6,28 +6,38 @@ import random
 playing_count = 0
 
 
+def deal():
+    global playing_count
+    print('Deal!')
+    print('\tRolling dice...')
+    die1 = roll_die()
+    die2 = roll_die()
+    print(f'\t\tDie #1  =  {die1}')
+    print(f'\t\tDie #2  =  {die2}')
+    playing_count = (die1 + die2) * 2
+
+
 def roll_die():
     return random.randint(1, 6)
 
 
 def hit():
-    playing_count = roll_die() * 2
+    global playing_count
     print('Hit!')
     print('\tRolling dice...')
     die1 = roll_die()
     die2 = roll_die()
     both = die1 + die2
-    print(f'\t\t1. Die #1    = {die1}.')
-    print(f'\t\t2. Die #2    = {die2}.')
+    print(f'\t\t1. Die #1  =  {die1}')
+    print(f'\t\t2. Die #2  =  {die2}')
     if playing_count + both > 21:
         num_choices = 2
     else:
-        print(f'\t\t3. Both dice = {both}.')
+        print(f'\t\t3. Both dice  =  {both}')
         num_choices = 3
     choice = 0  # Force while condition to avoid repeated code
     while choice not in range(1, num_choices+1):
-        choice = int(input('Choose one of the above options: '))
-    global playing_count  # We are able to access this variable everywhere in this code
+        choice = int(input('\tChoose one of the above options: '))
     if choice == 1:
         playing_count += die1
     elif choice == 2:
@@ -37,6 +47,16 @@ def hit():
 
 
 def main():
+    global playing_count
+    print('Blackjack\n')
+    deal()
+    print(f'Initial playing count = {playing_count}\n')
+
+    hit_or_stand = input('Type "hit" or "stand": ')
+    while hit_or_stand == 'hit':
+        hit()
+        print(f'Playing count = {playing_count}\n')
+        hit_or_stand = input('Type "hit" or "stand": ')
     return
 
 
