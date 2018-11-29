02552ffdd224c951c81c4aeef3e25593e17ffca5
sabrupu <sabrupu18@gmail.com>
2018-11-28 22:14:21 -0500

    Commit Lab 8.

---
+++ Lab8/Problem1.py
@@ -0,0 +1,23 @@
+# Write a function that will count the # of characters in the supplied string, and sum the
+# value of all integers.
+# Input: “he34l5lo8”
+# Output: There are 5 characters. The sum of all integers is 20
+# .isdigit
+
+
+def main():
+    x = "he34l5lo8"
+    letter = 0
+    number = 0
+
+    for i in range(x):
+        if letter.isalpha:
+            letter += 1
+        else:
+            number.isdigit
+            number += 1
+
+    print(len(x))
+
+
+main()
---
+++ Lab8/Problem2.py
@@ -0,0 +1,9 @@
+#  Write a function that will validate a user’s password:
+#  At least 1 letter between [a-z] and 1 letter between [A-Z].
+#  At least 1 number between [0-9].
+#  At least 1 character from [$#@].
+#  Minimum length 6 characters.
+#  Maximum length 16 characters.
+
+
+
