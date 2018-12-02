c12f75a2461c630ea11a7cefddc209e5577327f9
sabrupu <sabrupu18@gmail.com>
2018-12-01 21:57:49 -0500

    Commit Homework 3, Problem 1.

---
+++ Homework3/Problem1.py
@@ -0,0 +1,34 @@
+# You will build a "Fortune cookie" program. For those of you not familiar with it, once
+# upon a time there was a command line program for power users which would give a
+# random saying printed on the command line.
+#
+# In your program you will
+#     ● Create a text file in your project folder with at least 20 "quirky sayings"/fortunes
+#       (the only requirement is that they be appropriate for display in class),
+#     ● Create a list by reading those 20 fortunes from your file
+#     ● Ask the user how many fortunes he/she wants to see.
+#         ○ store the value in a variable
+#     ● Inside of a loop (loop as many times as the user asked)
+#       ● select a random answer from your list of fortunes
+#       ● wait for the user to press the enter key (the easy way is to use the input
+#          method)
+#
+# Note that it is quite possible that you will see some fortunes more than once while not
+# seeing some at all.
+
+
+import random
+
+# Read fortune list from file
+file = open('Fortunes.txt', 'r')
+fortunes = file.readlines()
+file.close()
+
+# Ask user how many fortunes he/she would like to see
+num_fortunes = int(input("How many fortunes would you like to see? "))
+
+# Print random fortunes
+for i in range(num_fortunes):
+    r = random.randrange(0, len(fortunes))
+    input()
+    print(fortunes[r])
