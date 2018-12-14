5a69ace6e653be17df7af589404ee146d45ce408
sabrupu <sabrupu18@gmail.com>
2018-12-13 23:36:50 -0500

    Commit Project2, Problem3.

---
+++ Project2/Problem3.py
@@ -0,0 +1,36 @@
+# Write a recursive function that will print all permutations of the given list. For the code
+# given, your output should be similar to the following:
+# [‘A’, ‘B’, ‘C’]
+# [‘A’, ‘C’, ‘B’]
+# [‘B’, ‘A’, ‘C’]
+# [‘B’, ‘C’, ‘A’]
+# [‘C’, ‘B’, ‘A’]
+# [‘C’, ‘A’, ‘B’]
+
+
+def compute_permutations(string, index):
+    if index >= len(string):
+        print(string)
+        return
+
+    for i in range(index, len(string)):
+        result = swap(string, index, i)  # Swapping element in string at index i
+        compute_permutations(result, index + 1)
+
+
+def swap(string, a, b):
+    elem1 = string[a]
+    elem2 = string[b]
+
+    result = string.copy()
+    result[a] = elem2
+    result[b] = elem1
+    return result
+
+
+def main():
+    string = ['A', 'B', 'C']
+    compute_permutations(string, 0)
+
+
+main()
